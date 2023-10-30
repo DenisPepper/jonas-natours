1a77a2598dc369823f87e95ba0d04730aa49ac13
DenisPepper <thech@yandex.ru>
2023-10-30 15:39:45 +0300

    Добавит роут для списка туров

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
+const fs = require('fs');
 const express = require('express');
 const port = 3000;
 const routes = {
   home: '/',
+  tours: '/api/v1/tours',
 };
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours.json`));
 
 const app = express();
 
+app.get(routes.tours, (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours,
+    },
+  });
+});
+
+/* 
 app.get(routes.home, (req, res) => {
   res.status(200).json({ message: 'welcome to home page', app: 'Natours app' });
 });
 
+app.post(routes.home, (req, res) => {
+  res.status(404).send('Stop doing this...');
+});
+ */
 app.listen(port, () => console.log(`server run on ${port} port ...`));
