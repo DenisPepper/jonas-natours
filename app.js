832e2d1296df30980c5607928632aa1a893dd6e4
DenisPepper <thech@yandex.ru>
2023-10-30 14:34:09 +0300

    Создаст корневой роут

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const port = 3000;
+const routes = {
+  home: '/',
+};
 
 const app = express();
-const port = '3000';
+
+app.get(routes.home, (req, res) => {
+  res.status(200).send('welcome to home page');
+});
 
 app.listen(port, () => console.log(`server run on ${port} port ...`));
