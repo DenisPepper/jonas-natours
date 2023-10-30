3bdfe2cf00f6af74297580f1a0bb1f99b8256dee
DenisPepper <thech@yandex.ru>
2023-10-30 21:28:48 +0300

    Добавит мидлваре для параметра

--- routes/tour-routes.js
+++ routes/tour-routes.js
@@ -2,6 +2,13 @@ const express = require('express');
 const tourController = require('../controllers/tour-controller');
 const router = express.Router();
 
+// это middleware сработает для параметра id
+router.param('id', (req, res, next, val) => {
+  console.log(`val is ${val}`);
+
+  next();
+});
+
 router.route('/').get(tourController.getTours).post(tourController.createTour);
 
 router
