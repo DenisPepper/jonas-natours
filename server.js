276e1a3c556b43869318c7e2881b5bb1f38c6bf5
DenisPepper <thech@yandex.ru>
2023-10-31 11:16:38 +0300

    Выведет в консоль переменные окружения

--- server.js
+++ server.js
@@ -3,3 +3,9 @@ const app = require('./app');
 const port = 3000;
 
 app.listen(port, () => console.log(`server run on ${port} port ...`));
+
+// переменные окружения node
+console.log(process.env);
+
+// переменные окружения express
+console.log(app.get('env'))
