952056a4c556f1264a907014065fbe228c3133d8
DenisPepper <thech@yandex.ru>
2023-11-02 20:39:01 +0300

    Расширит структуру модели туров

--- models/tour-model.js
+++ models/tour-model.js
@@ -5,11 +5,43 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Tour must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: { type: Number, default: 4.5 },
+  duration: { type: Number, required: [true, 'Tour must have a duration'] },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'Tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'Tour must have a difficulty'],
+  },
+  ratingsAverage: { type: Number, default: 4.5 },
+  ratingsQuantity: { type: Number, default: 0 },
   price: { type: Number, required: [true, 'Tour must have a price'] },
+  priceDiscount: Number,
+  summery: {
+    type: String,
+    trim: true,
+    required: [true, 'Tour must have a summery'],
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: [true, 'Tour must have a description'],
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'Tour must have an image cover'],
+  },
+  images: [String],
+  createdAt: { type: Date, default: Date.now() },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
+
+// trim: true - mongo будет тримить с обоих сторон строковые значения
+// unique: true - mongo будет контролировать уникальность значений этого ключа
