5bf396bf16404ab7a032297237e3d815a3814468
DenisPepper <thech@yandex.ru>
2023-11-05 11:25:07 +0300

    Добавит сортировку по умолчанию

--- controllers/tour-controller.js
+++ controllers/tour-controller.js
@@ -25,6 +25,8 @@ exports.getTours = async (req, res) => {
   if (req.query.sort) {
     const sortBy = req.query.sort.split(',').join(' ');
     query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt'); // default sorting
   }
 
   // <-- BUILD QUERY
