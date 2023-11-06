9423e23f68c2bdb94d4b5090983c244961c66ec1
DenisPepper <thech@yandex.ru>
2023-11-06 08:12:59 +0300

    Вычислит статистику с помощью mongo aggregate pipeline

--- controllers/tour-controller.js
+++ controllers/tour-controller.js
@@ -104,7 +104,7 @@ exports.deleteTour = async (req, res) => {
 
 exports.getTourStats = async (req, res) => {
   try {
-    const stats = Tour.aggregate([
+    const stats = await Tour.aggregate([
       { $match: { ratingsAverage: { $gte: 4.5 } } },
       {
         $group: {
