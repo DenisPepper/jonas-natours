68ad236237065e4e4c63e4885fcab67e3fb097ac
DenisPepper <thech@yandex.ru>
2023-11-07 18:17:49 +0300

    Доработает механизм отправки ошибок

--- controllers/error-controller.js
+++ controllers/error-controller.js
@@ -1,10 +1,37 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    error: err,
+    status: err.status,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendError = (err, res) => {
+  if (err.isOperational) {
+    //trusted operational app error
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    // unknow programming error
+    console.error('programming error!🐸', err);
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong!',
+    });
+  }
+};
+
 const globalErrorHandler = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+
+  if (process.env.NODE_ENV === 'development') sendErrorDev(err, res);
+
+  if (process.env.NODE_ENV === 'production') sendError(err, res);
+
   next();
 };
 
