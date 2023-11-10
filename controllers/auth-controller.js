2f07f5303a2bcb7402079f56eb182cfca0cd33ae
DenisPepper <thech@yandex.ru>
2023-11-10 21:25:35 +0300

    Передаст в db при создании нового юзера только нужные поля

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -2,7 +2,12 @@ const User = require('../models/user-model');
 const handleAsync = require('../utils/handle-async');
 
 exports.signup = handleAsync(async (req, res, next) => {
-  const user = await User.create(req.body);
+  const user = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
   res.status(201).json({
     status: 'success',
     data: {
