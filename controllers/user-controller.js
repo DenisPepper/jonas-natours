fe2652b38cfa7bc01bad5c566fbdc741e87013f9
DenisPepper <thech@yandex.ru>
2023-11-14 17:15:34 +0300

    Реализует метод запроса пользователей

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,24 @@
-const getStub = (req, res) => {
-  res.status(500).send('server error!');
-};
+const User = require('../models/user-model');
+const APIFeatures = require('../utils/api-features');
+const handleAsync = require('../utils/handle-async');
+
+const getStub = handleAsync(async (req, res, next) => {
+  const features = new APIFeatures(User.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+
+  const users = await features.query;
+
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
+  });
+});
 
 exports.getAllUsers = (req, res, next) => getStub(req, res);
 
