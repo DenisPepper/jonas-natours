703d6a3e56ae6602bbab400d61c44166823a65f5
DenisPepper <thech@yandex.ru>
2023-11-16 09:35:21 +0300

    Создаст единый метод авторизации пользователя

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -11,15 +11,8 @@ const signToken = (id) =>
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 
-exports.signup = handleAsync(async (req, res, next) => {
-  const user = await User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-    passwordConfirm: req.body.passwordConfirm,
-  });
-
-  res.status(201).json({
+const createSendToken = (user, statusCode, res) => {
+  res.status(statusCode).json({
     status: 'success',
     token: signToken(user._id),
     data: {
@@ -28,6 +21,17 @@ exports.signup = handleAsync(async (req, res, next) => {
       role: user.role,
     },
   });
+};
+
+exports.signup = handleAsync(async (req, res, next) => {
+  const user = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  createSendToken(user, 201, res);
 });
 
 exports.login = handleAsync(async (req, res, next) => {
@@ -45,15 +49,7 @@ exports.login = handleAsync(async (req, res, next) => {
   if (!user || !(await user.comparePasswords(password, user.password)))
     return next(new AppError('email or password not valid!', 401));
 
-  res.status(200).json({
-    status: 'success',
-    token: signToken(user._id),
-    data: {
-      name: user.name,
-      email: user.email,
-      role: user.role,
-    },
-  });
+  createSendToken(user, 200, res);
 });
 
 exports.protect = handleAsync(async (req, res, next) => {
@@ -171,11 +167,5 @@ exports.resetPassword = handleAsync(async (req, res, next) => {
   await user.save();
 
   // 3 Логиним пользователя и отправляем новый JWT
-  res.status(201).json({
-    status: 'success',
-    token: signToken(user._id),
-    data: {
-      user: { ...user, password: undefined },
-    },
-  });
+  createSendToken(user, 201, res);
 });
