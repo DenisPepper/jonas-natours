b69766ec03c1b50480af62dc053151febb86363e
DenisPepper <thech@yandex.ru>
2023-11-20 14:06:50 +0300

    Создаст модель отзыва

---
+++ models/review-model.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review can`t be empty!'],
+      trim: true,
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review can`t be without ref to user!'],
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review can`t be without ref to tour!'],
+    },
+    rating: {
+      type: Number,
+      max: [5, 'Tour rating must be <= 5'],
+      min: [1, 'Tour rating must be >= 1'],
+    },
+    createdAt: { type: Date, default: Date.now() },
+  },
+  // включает виртуальные свойства
+  { toJSON: { virtuals: true } },
+  { toObject: { virtuals: true } },
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
