6adc8cf892c4e7fe964469752f2821a0adb267bb
DenisPepper <thech@yandex.ru>
2023-11-24 12:43:56 +0300

    Добавит вывод карточек туров в цикле

--- controllers/view-controller.js
+++ controllers/view-controller.js
@@ -1,6 +1,16 @@
-exports.getOverview = (req, res, next) => {
-  res.status(200).render('overview', { title: 'All tours' }); // передаст в шаблон пропсы
-};
+const Tour = require('../models/tour-model');
+const handleAsync = require('../utils/handle-async');
+
+exports.getOverview = handleAsync(async (req, res, next) => {
+  // запрос туров из БД
+  const tours = await Tour.find();
+
+  // пстроение шаблона вьюшки
+
+  // рендер шаблона с данными о турах
+
+  res.status(200).render('overview', { title: 'All tours', tours }); // передаст в шаблон пропсы
+});
 
 exports.getTour = (req, res, next) => {
   res.status(200).render('tour', { title: 'The Forest Hiker Tour' }); // передаст в шаблон пропсы
