0c474a3e78a64456e23cd607239a103bc97a1b8d
DenisPepper <thech@yandex.ru>
2023-11-27 18:46:31 +0300

    Исправит механизм представления ошибок в dev режиме

--- controllers/error-controller.js
+++ controllers/error-controller.js
@@ -38,9 +38,9 @@ const globalErrorHandler = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  if (process.env.NODE_ENV === 'development') sendErrorDev(err, res);
+  if (process.env.NODE_ENV === 'development') sendErrorDev(err, req, res);
 
-  if (process.env.NODE_ENV === 'production') sendError(err, res);
+  if (process.env.NODE_ENV === 'production') sendError(err, req, res);
 
   next();
 };
